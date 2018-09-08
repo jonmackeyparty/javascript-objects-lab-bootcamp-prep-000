db7ce6f327dd07305b036e41b7cff96d3bd1c589
Jon Mackey <jon@jonmackey.party>
2018-09-08 19:32:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {prop: 1};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, recipes, { prop2: 2 });
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  recipes.prop2 = '2';
+  return recipes;
+}
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign ({}, recipes);
+  delete recipes.prop;
+  return recipes;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete recipes.prop;
+  return recipes;
+}
