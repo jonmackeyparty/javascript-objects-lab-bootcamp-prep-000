c77aaad8f99f778084cdca49ede50619dc34cd4b
Jon Mackey <jon@jonmackey.party>
2018-09-08 19:35:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
-  return recipes;
+  return object;
 }
+
+destructivelyUpdateObjectWithKeyAndValue
 
 function deleteFromObjectByKey(object, key){
   var newObj = Object.assign ({}, recipes);
