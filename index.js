200986e432ccf778c7e0b659f348ebdf3a3854d6
Jon Mackey <jon@jonmackey.party>
2018-09-08 19:34:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key]
+  object[key] = value;
   return recipes;
 }
 
